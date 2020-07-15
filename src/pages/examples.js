8dd3799f141447ce72b4126e951ca3de1d3f69f3
Adam Turner <adam.turner@and.digital>
2020-07-15 13:30:46 +0100

    Fixed typos looking for layout file

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,14 +1,14 @@
 import React from "react"
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Header from "../examples/Header"
 
 const examples = () => {
-  return (
-    <Layout>
-      <h1>Hello from examples</h1>
-      <Header />
-    </Layout>
-  )
+    return (
+        <Layout>
+            <h1>Hello from examples</h1>
+            <Header />
+        </Layout>
+    )
 }
 
 export default examples
